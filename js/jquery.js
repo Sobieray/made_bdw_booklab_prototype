86d0a89748c020e4a39d2055e23a31b470658b06
Your Name <you@domain.com>
2013-06-28 13:45:15 -0600

    Photos are complete.

--- js/jquery.js
+++ js/jquery.js
@@ -7,17 +7,45 @@ $(document).ready(function() {
 
 $('.bike').live('click', function(){
   $('#made').css('display', '');
-  $('#city').fadeIn(1800, function(){
+  $('#city').fadeIn(2000, function(){
     //
   });
   $('#made').addClass('hidden');
 });
 
-
-$('.woods').live('click', function(){
+$('.fancy').live('click', function(){
   $('#city').css('display', '');
-  $('#woods').fadeIn(1800, function(){
+  $('#bike_fancy').fadeIn(1500, function(){
     //
   });
   $('#city').addClass('hidden');
+});
+
+
+/* CHANGING FROM BIKES TO WOODS */
+
+$('.woods').live('click', function(){
+  $('#city').css('display', '');
+  $('#woods').fadeIn(1500, function(){
+    //
+  });
+  $('#city').addClass('hidden');
+});
+
+$('.comfortable').live('click', function(){
+  $('#woods').css('display', '');
+  $('#outdoor_comfy').fadeIn(1500, function(){
+    //
+  });
+  $('#woods').addClass('hidden');
+});
+
+/* CHANGING FROM MADE TO WOODS */
+
+$('.woods').live('click', function(){
+  $('#made').css('display', '');
+  $('#woods').fadeIn(1800, function(){
+    //
+  });
+  $('#made').addClass('hidden');
 });
