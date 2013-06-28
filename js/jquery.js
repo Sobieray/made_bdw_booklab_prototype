330b8e180dc9cb1d4d1809e5752b72339c518b1e
Your Name <you@domain.com>
2013-06-28 11:01:41 -0600

    It works

--- js/jquery.js
+++ js/jquery.js
@@ -1,11 +1,12 @@
-$(document).ready(function() {
-  $('.isMade').fadeIn(2500, function(){
-    //Animation complete
-  });
-});
+// $(document).ready(function() {
+//   $('#made').fadeIn(2500, function(){
+//     //Animation complete
+//   });
+// });
 
 
-$('#cd-dropdown3').change(function(){
-  var value = $("#cd-dropdown3 option:selected").val(City);
-  console.log(this)
+$('.bike').live('click', function(){
+  $('#city').removeClass();
+  $('#made').addClass('hidden');
 });
+
